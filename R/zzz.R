2195c0fd48cbb4d20addfb135d2a0cfc5efc2ffb
jacpete <jacobmpeterson12@gmail.com>
2021-01-04 22:14:51 -0600

    prevent documentation of .onAttach

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,6 @@
 
 #' On attach, if on a MARC computer with Covid19MARCInternal installed, check for updates to datasets. If there are updates print a message
+#' @noRd
 .onAttach <- function(...) {
     checkPublicStaticDatasets()
 }
