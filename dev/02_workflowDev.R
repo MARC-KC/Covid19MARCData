6a2e90b0d666d4251f29580309f886736a103fd1
jacpete <jacobmpeterson12@gmail.com>
2021-01-06 00:15:30 -0600

    add cdtHosp combination

--- dev/02_workflowDev.R
+++ dev/02_workflowDev.R
@@ -4,4 +4,32 @@
 cdtData <- downloadBaseData(type = "CDT")
 
 cdtNRData <-  downloadBaseData(type = "CDT_NewlyReported")
+
+hospData <-  downloadBaseData(type = "Hospital")
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Combine Hospital and CDT Base Data ####
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+cdtHospData <- dplyr::full_join(cdtData, hospData, by = c("GeoID", "Date")) %>%
+    marcR::coalesceJoin() %>%
+    dplyr::mutate(CovidNew24H = CovidNew24HConfirmed + CovidNew24HSuspected) %>%
+    dplyr::select(
+        Jurisdiction, State, GeoID, Region, Date,
+
+        CasesNew, CasesTotal,
+        DeathsNew, DeathsTotal,
+        TestsNew, TestsTotal,
+        Population,
+
+        HospitalsReporting, HospitalsTotal,
+        BedsTotal, BedsUsed, BedsAvailable,
+        BedsICUTotal, BedsICUUsed, BedsICUAvailable,
+        CovidTotal, CovidConfirmed, CovidSuspected,
+        CovidNew24H, CovidNew24HConfirmed, CovidNew24HSuspected,
+        CovidICUTotal, CovidICUConfirmed, CovidICUSuspected,
+        VentilatorsTotal, VentilatorsUsed, VentilatorsAvailable,
+        CovidVentilatorsUsed)
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
