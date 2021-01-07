5219c8a368fe504134b1106dbc32afe998a7870b
jacpete <jacobmpeterson12@gmail.com>
2021-01-07 14:02:45 -0600

    created list2CSV and made some formatting changes

--- dev/02_workflowDev.R
+++ dev/02_workflowDev.R
@@ -14,11 +14,14 @@ lagDaysHosp = 2
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     # Load in CDT and Hospital Base Data ####
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-cdtData <- downloadBaseData(type = "CDT")
+cdtData <- downloadBaseData(type = "CDT") %>%
+    dplyr::filter(GeoID %in% GeoIDs[['base']])
 
-cdtNRData <-  downloadBaseData(type = "CDT_NewlyReported")
+cdtNRData <-  downloadBaseData(type = "CDT_NewlyReported") %>%
+    dplyr::filter(GeoID %in% GeoIDs[['base']])
 
-hospData <-  downloadBaseData(type = "Hospital")
+hospData <-  downloadBaseData(type = "Hospital") %>%
+    dplyr::filter(GeoID %in% GeoIDs[['base']])
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 
@@ -40,8 +43,8 @@ cdtHospData <- dplyr::full_join(cdtData, hospData, by = c("GeoID", "Date")) %>%
         HospitalsReporting, HospitalsTotal,
         BedsTotal, BedsUsed, BedsAvailable,
         BedsICUTotal, BedsICUUsed, BedsICUAvailable,
-        CovidTotal, #CovidConfirmed, CovidSuspected, ##############DELETE COMMENT ####
-        CovidNew, #CovidNew24HConfirmed, CovidNew24HSuspected, ##############DELETE COMMENT ####
+        CovidTotal,
+        CovidNew,
         CovidICUTotal, CovidICUConfirmed, CovidICUSuspected,
         VentilatorsTotal, VentilatorsUsed, VentilatorsAvailable,
         CovidVentilatorsUsed)
@@ -85,10 +88,11 @@ cdtHosp14DayRollingData <- rollSummaryXDays(df = cdtHospData, numDays = 14, varT
 # Add Base Tables ####
 cat(crayon::blue("Exporting base CDT data.\n"))
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-bi_CDT_TimeSeries <- cdtData %>%
-    dplyr::filter(GeoID %in% GeoIDs[['base']])
+bi_CDT_TimeSeries <- cdtData
 
-bi_CDT_MostRecent <- bi_CDT_TimeSeries %>% marcR::groupby_rank(GeoID, rankby = Date, filterIDs = 1)
+bi_CDT_NewlyReported <- cdtNRData %>% marcR::groupby_rank(GeoID, rankby = Date, filterIDs = 1)
+
+# bi_CDT_MostRecent <- bi_CDT_TimeSeries %>% marcR::groupby_rank(GeoID, rankby = Date, filterIDs = 1)
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 
@@ -118,8 +122,7 @@ bi_7DayRolling <-
             TestsNew7DayAvg < PopulationTestStandard ~ 3,
             TestsNew7DayAvg > PopulationTestStandard ~ 1,
             TRUE ~ 2
-        )) %>%
-    dplyr::filter(GeoID %in% GeoIDs[['base']])
+        ))
 
 
 # bi_7DayRollingLag <- bi_7DayRolling %>% dplyr::filter(Date <= (max(Date) - lagDays))
@@ -186,9 +189,7 @@ measureTable <- tibble::tribble(
 
 
 
-baseWeeklyComparisonData <- baseDaysComparison(cdtHosp7DayRollingData, measureTable) %>%
-    dplyr::filter(GeoID %in% GeoIDs[['base']])
-
+baseWeeklyComparisonData <- baseDaysComparison(cdtHosp7DayRollingData, measureTable)
 
 
 bi_7DayComparison_MostRecent <- baseWeeklyComparisonData %>%
@@ -299,7 +300,7 @@ mostRecentGivenHelperTable <- tibble::tribble(
     "cdtHospData",        30,    lagDays,  "Both",
     "cdtHospData",        60,    lagDays,  "Both",
     "cdtHospData",        90,    lagDays,  "Both",
-    "bi_CDT_MostRecent",  NA,    NA,       "Both"
+    "cdtHospData",        NA,    NA,       "Both"
 )
 
 bi_JurisdictionBarCharts <- purrr::pmap_dfr(mostRecentGivenHelperTable, function(datasetName, days, lagDays, keep, ...) {
@@ -320,7 +321,6 @@ bi_JurisdictionBarCharts <- purrr::pmap_dfr(mostRecentGivenHelperTable, function
 })
 
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
 
 
 
@@ -354,39 +354,57 @@ bi_HelperTable <- tibble::tribble(
 
 
 
-outputFolderName = '//KCJazz/GIS/DataDevelopment/HumanServices/COVID-19/Outputs/PipelineDataOutputs/PublishData'
+
+out <- mget(stringr::str_subset(ls(), "^bi_"))
+
+
+
+
+# outputFolderName = '//KCJazz/GIS/DataDevelopment/HumanServices/COVID-19/Outputs/PipelineDataOutputs/PublishData'
+#
+#
+#
+#
+# #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# # Export Power Bi datasets to CSVs ####
+# #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# biObjects <- ls() %>% stringr::str_subset("^bi_")
+#
+# cat(crayon::green("Exporting", length(biObjects), paste0("csv files to '", outputFolderName, "\n")))
+# if (dir.exists(outputFolderName)) unlink(outputFolderName, recursive = TRUE)
+# dir.create(outputFolderName, recursive = TRUE, showWarnings = FALSE)
+# purrr::walk(seq_along(biObjects), ~{
+#     fileName <- biObjects[.x] %>% stringr::str_remove("^bi_")
+#     cat(crayon::blue("Saving file:", paste0(fileName, ".csv"), paste0("(", .x, "/", length(biObjects), ")\n")))
+#     readr::write_csv(x = eval(parse(text=paste0("as.data.frame(", biObjects[.x], ")"))), file = file.path(outputFolderName, paste0(fileName, ".csv")), na = "")
+# })
+# cat(crayon::green("Export Completed Successfully\n"))
+# #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
 
 
 
 
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# Export Power Bi datasets to CSVs ####
-#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-biObjects <- ls() %>% stringr::str_subset("^bi_")
-
-cat(crayon::green("Exporting", length(biObjects), paste0("csv files to '", outputFolderName, "\n")))
-if (dir.exists(outputFolderName)) unlink(outputFolderName, recursive = TRUE)
-dir.create(outputFolderName, recursive = TRUE, showWarnings = FALSE)
-purrr::walk(seq_along(biObjects), ~{
-    fileName <- biObjects[.x] %>% stringr::str_remove("^bi_")
-    cat(crayon::blue("Saving file:", paste0(fileName, ".csv"), paste0("(", .x, "/", length(biObjects), ")\n")))
-    readr::write_csv(x = eval(parse(text=paste0("as.data.frame(", biObjects[.x], ")"))), file = file.path(outputFolderName, paste0(fileName, ".csv")), na = "")
-})
-cat(crayon::green("Export Completed Successfully\n"))
+# Export List of Datasets to CSVs ####
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 
+list2CSV <- function(objectList, outputFolder) {
+    message(crayon::green("Exporting", length(objectList), paste0("csv files to '", outputFolder, "\n")))
+    if (dir.exists(outputFolder)) unlink(outputFolder, recursive = TRUE) #if folder exists, delete it
+    dir.create(outputFolder, recursive = TRUE, showWarnings = FALSE) #create the directory
+    purrr::walk(seq_along(objectList), ~{
+        fileName <- stringr::str_remove(names(objectList)[.x], "^bi_")
+        message(crayon::blue("Saving file:", paste0(fileName, ".csv"), paste0("(", .x, "/", length(objectList), ")\n")))
+        readr::write_csv(x = objectList[[.x]], file = file.path(outputFolder, paste0(fileName, ".csv")), na = "")
+    })
+    message(crayon::green("Export Completed Successfully\n"))
+}
 
-
-
-
-
-
-
-
-
-
-
-
+objectList = out
+outputFolder = '//KCJazz/GIS/DataDevelopment/HumanServices/COVID-19/Outputs/PipelineDataOutputs/PublishData'
+list2CSV(objectList, outputFolder)
 
 
