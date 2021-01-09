3ae60ad098b372f45ab6f55f07d14d5ab9e56f53
jacpete <jacobmpeterson12@gmail.com>
2021-01-08 22:00:48 -0600

    update list2CSV to be less dangerous with the deleting of the output folder

--- R/list2CSV.R
+++ R/list2CSV.R
@@ -2,12 +2,13 @@
 #' @title Export List to CSVs
 #'
 #' @param dfList A named list of data.frames for export
-#' @param outputFolder The file path to the folder that the CSV's will be exported to.
+#' @param outputFolder The file path to the folder that the CSV's will be exported to. Defaults to \code{file.path(getwd(), 'outputDatasetsForPowerBi')}
+#' @param overwriteDirectory Should the directory be deleted and recreated? Defaults to FALSE as this is a dangerous option
 #'
 #' @export
-list2CSV <- function(dfList, outputFolder) {
+list2CSV <- function(dfList, outputFolder = file.path(getwd(), 'outputDatasetsForPowerBi'), overwriteDirectory = FALSE) {
     message(crayon::yellow("Exporting", length(dfList), paste0("csv files to '", outputFolder)))
-    if (dir.exists(outputFolder)) unlink(outputFolder, recursive = TRUE) #if folder exists, delete it
+    if (dir.exists(outputFolder) & overwriteDirectory) unlink(outputFolder, recursive = TRUE) #if folder exists, delete it
     dir.create(outputFolder, recursive = TRUE, showWarnings = FALSE) #create the directory
     purrr::walk(seq_along(dfList), ~{
         fileName <- stringr::str_remove(names(dfList)[.x], "^bi_")
